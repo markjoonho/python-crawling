e848d5ae4876ed61d15ed8d9c0722b499dc0f1f4
Mark Joonho Lee <j669lee@uwaterloo.ca>
2020-12-20 17:12:31 +0900

    201220 python crawling project

---
+++ indeed.py
@@ -0,0 +1,53 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_last_page(url):
+    result = requests.get(url)
+    soup = BeautifulSoup(result.text, "html.parser")
+    pagination = soup.find("div", {"class": "pagination"})
+    links = pagination.find_all("a")
+    pages = []
+    for link in links[:-1]:
+        pages.append(int(link.find("span").text))
+    pages = pages
+    max_page = pages[-1]
+    return max_page
+
+
+def extract_job(html):
+    title = (html.find("h2", {"class": "title"})).find("a")["title"]
+    company = html.find("span", {"class": "company"})
+    company_anchor = company.find("a")
+    if company_anchor is not None:
+        company = str(company_anchor.string)
+    else:
+        company = str(company.string)
+    company = company.strip("\n")
+    title = title.strip("\n")
+    location = html.find("div", {'class': 'recJobLoc'})["data-rc-loc"]
+    job_id = html["data-jk"]
+    return {'title': title, 'company': company, 'location': location, "link": "https://www.indeed.com/viewjob?jk={}".format(job_id)}
+
+
+def extract_jobs(last_page, url):
+    jobs = []
+    for page in range(last_page):
+        print("scrapping page : {}".format(page))
+        new_url = "{}&start={}".format(url, page*50)
+        result = requests.get(new_url)
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+
+        for result in results:
+            job = extract_job(result)
+            jobs.append(job)
+
+    return jobs
+
+
+def get_jobs(word):
+    url = "https://www.indeed.com/jobs?q={}&limit=50".format(word)
+    last_page = get_last_page(url)
+    jobs = extract_jobs(last_page, url)
+    return jobs
